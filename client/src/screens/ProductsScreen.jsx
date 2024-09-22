094351fb15a777fa0e952d4ab57b7ff08fd131b5
unknown <experte001@gmail.com>
2024-09-22 22:34:23 +0900

    Product Card has been designed

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,7 +1,45 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Wrap, WrapItem } from "@chakra-ui/react";
+import ProductCard from "../components/ProductCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductsScreen() {
-  return <Box>Chakra Box</Box>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/products")
+      .then((res) => {
+        setData(res.data.products);
+      })
+      .catch((error) => {
+        console.error("Failed to fetching data! \n Error:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {data.length > 1 && (
+        <Box>
+          <Wrap
+            spacing="30px"
+            justify="center"
+            minHeight="80vh "
+            mx={{ base: "12", md: "20", lg: "32" }}
+          >
+            {/* <ProductCard product={data[0]} loading={false} /> */}
+            {data.map((product) => (
+              <WrapItem key={product._id}>
+                <Center w={"250px"} h={"450px"}>
+                  <ProductCard product={product} loading={false} />
+                </Center>
+              </WrapItem>
+            ))}
+          </Wrap>
+        </Box>
+      )}
+    </>
+  );
 }
 
 export default ProductsScreen;
