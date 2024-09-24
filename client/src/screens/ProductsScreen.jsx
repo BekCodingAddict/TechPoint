f67407e260ef12bb2977030adcfe884a58a58da0
unknown <experte001@gmail.com>
2024-09-25 01:40:31 +0900

    Pagination & Favorites has been displayed on  ProductScreen!

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,22 +1,27 @@
-import { Box, Center, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Button, Center, Wrap, WrapItem } from "@chakra-ui/react";
 import ProductCard from "../components/ProductCard";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "../redux/actions/productActions";
 
+import { MdArrowLeft, MdArrowRight } from "react-icons/md";
+
 function ProductsScreen() {
   const dispatch = useDispatch();
-  const { loading, error, products, pagination } = useSelector(
-    (state) => state.product
-  );
+  const { loading, error, products, pagination, favoritesToggled } =
+    useSelector((state) => state.product);
 
   useEffect(() => {
     dispatch(getProducts(1));
   }, [dispatch]);
+
+  const paginationButtonClick = (page) => {
+    dispatch(getProducts(page));
+  };
   return (
     <>
-      {products.length > 1 && (
+      {products.length >= 1 && (
         <Box>
           <Wrap
             spacing="30px"
@@ -33,6 +38,36 @@ function ProductsScreen() {
               </WrapItem>
             ))}
           </Wrap>
+          {!favoritesToggled && (
+            <Wrap spacing={"10px"} justify={"center"} p={"5"}>
+              <Button
+                colorScheme="cyan"
+                onClick={() => paginationButtonClick(1)}
+              >
+                <MdArrowLeft />
+              </Button>
+
+              {Array.from(Array(pagination.totalPages), (e, i) => {
+                return (
+                  <Button
+                    colorScheme={
+                      pagination.currentPage === i + 1 ? "cyan" : "gray"
+                    }
+                    key={i}
+                    onClick={() => paginationButtonClick(i + 1)}
+                  >
+                    {i + 1}
+                  </Button>
+                );
+              })}
+              <Button
+                colorScheme="cyan"
+                onClick={() => paginationButtonClick(pagination.totalPages)}
+              >
+                <MdArrowRight />
+              </Button>
+            </Wrap>
+          )}
         </Box>
       )}
     </>
