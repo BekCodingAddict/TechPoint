c544623cc28c4992b3fe8e92b336210c2d83b48c
unknown <experte001@gmail.com>
2024-09-28 21:37:23 +0900

    Review Schema has been created!

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,5 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    title: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const productSchema = new mongoose.Schema(
   {
     name: {
@@ -19,11 +32,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       require: true,
     },
-    reviews: {
-      type: Array,
-      require: true,
-      default: [],
-    },
+    reviews: [reviewSchema],
     numOfReviews: {
       type: Number,
       require: true,
